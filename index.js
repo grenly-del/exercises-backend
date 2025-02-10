1a8aa7c7de6aa2805306ec4dec6dd5781a70b885
grenly-del <ardiand400@gmail.com>
2025-02-10 09:52:47 +0800

    menambahkan request dengan params, dan juga request dengan semua method

--- index.js
+++ index.js
@@ -3,8 +3,53 @@ const express = require('express')
 const app = express()
 
 
+// Routing
+app.get('/', (req, res) => {
+    res.write('Hello World')
+    res.end()
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).json({
+        status: "success",
+        message: "About page",
+        data: []
+    })
+})
+
+app.patch('/contoh', (req, res) => {
+    res.send('Request contoh dengan method PATCH')  
+})
+app.post('/contoh', (req, res) => {
+    res.send('Request contoh dengan method POST')
+})
+
+app.put('/contoh', (req,res) => {
+    res.send('Request contoh dengan PUT')
+})
+
+app.delete('/contoh', (req, res) => {
+    res.send('Request contoh dengan Delete')
+})
+
+// Routing Semua Method
+app.all('/universal', (req, res) => {
+    res.send(`Request method ${req.method}`)
+})
+
+// Routing Dinamis
+// 1. Menggunakan params
+
+app.get('/post/:id', (req, res) => {
+    res.send(`Artikel ke - ${req.params.id}`)
+})
 
 
+
+
+
+
+// Config Server
 
 const hostname = '127.0.0.1'
 const port = 3002
